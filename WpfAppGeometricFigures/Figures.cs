204c7d019d6ac13e2addf4cbb05c4c8726494606
Andrey Yakovlev <andreyyakovlevindex@outlook.com>
2023-12-08 23:28:12 +0300

    Добавьте файлы проекта.

---
+++ WpfAppGeometricFigures/Figures.cs
@@ -0,0 +1,82 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfAppGeometricFigures
+{
+    internal class Figures
+    {
+        Random random = new Random();
+        Polygon myPolygon = new Polygon();
+
+        // Random number for figures
+        public int choiceXNameNumber;
+
+        // Random coordinates for polygon
+        public int firstCoordinate;
+        public int secondCoordinate;
+
+        private MainWindow mainWindow;
+
+        public Figures(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+        }
+
+        public void ShowFigure()
+        {
+            mainWindow._1.Visibility = Visibility.Hidden;
+            mainWindow._2.Visibility = Visibility.Hidden;
+            mainWindow._3.Visibility = Visibility.Hidden;
+            mainWindow._4.Visibility = Visibility.Hidden;
+
+            choiceXNameNumber = random.Next(1, 5);
+
+            if (choiceXNameNumber == 1)
+                mainWindow._1.Visibility = Visibility.Visible;
+            else if (choiceXNameNumber == 2)
+                mainWindow._2.Visibility = Visibility.Visible;
+            else if (choiceXNameNumber == 3)
+                mainWindow._3.Visibility = Visibility.Visible;
+            else if (choiceXNameNumber == 4)
+                mainWindow._4.Visibility = Visibility.Visible;
+        }
+
+        public void ShowPolygon()
+        {
+            mainWindow._5.Visibility = Visibility.Visible;
+
+            // Когда создается экземпляр newPoints объекта PointCollection, Polygon в XAML сбрасывается до исходных значений
+            PointCollection newPoints = new PointCollection();
+
+            myPolygon = mainWindow._5;
+
+            firstCoordinate = random.Next(20, 300);
+            secondCoordinate = random.Next(20, 300);
+            newPoints.Add(new Point(firstCoordinate, secondCoordinate));
+
+            firstCoordinate = random.Next(20, 300);
+            secondCoordinate = random.Next(20, 300);
+            newPoints.Add(new Point(firstCoordinate, secondCoordinate));
+
+            firstCoordinate = random.Next(20, 300);
+            secondCoordinate = random.Next(20, 300);
+            newPoints.Add(new Point(firstCoordinate, secondCoordinate));
+
+            firstCoordinate = random.Next(20, 300);
+            secondCoordinate = random.Next(20, 300);
+            newPoints.Add(new Point(firstCoordinate, secondCoordinate));
+
+            myPolygon.Points = newPoints;
+        }
+
+        public void ClearFigures()
+        {
+            mainWindow._1.Visibility = Visibility.Hidden;
+            mainWindow._2.Visibility = Visibility.Hidden;
+            mainWindow._3.Visibility = Visibility.Hidden;
+            mainWindow._4.Visibility = Visibility.Hidden;
+            mainWindow._5.Visibility = Visibility.Hidden;
+        }
+    }
+}
---
+++ WpfAppGeometricFigures/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+
+namespace WpfAppGeometricFigures
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Figures figures;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            figures = new Figures(this);
+        }
+
+        /// <summary>
+        /// При нажатии на кнопку, окно Windows будет отображать случайно выбранную фигуру, такую как: Ellipse, Rectangle (1), Rectangle (2) или Polygon.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RandomFigure_Click(object sender, RoutedEventArgs e)
+        {
+            figures.ShowFigure();
+        }
+
+        /// <summary>
+        /// При нажатии на кнопку, окно Windows будет отображать Polygon со случайными координатами.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RandomPolygon_Click(object sender, RoutedEventArgs e)
+        {
+            figures.ShowPolygon();
+        }
+
+        /// <summary>
+        /// Очистить фигуры в окне Windows.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ClearWindow_Click(object sender, RoutedEventArgs e)
+        {
+            figures.ClearFigures();
+        }
+
+        /// <summary>
+        /// Закрыть окно Windows.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CloseWindow_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
